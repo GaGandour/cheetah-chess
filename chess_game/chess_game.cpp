fd524de8eca9a69da406c7895b8b2e64084f7329
Gabriel Gandour <gagandour@gmail.com>
2023-10-22 14:07:26 -0300

    init chess game interface

--- chess_game/chess_game.cpp
+++ chess_game/chess_game.cpp
@@ -1,42 +1,63 @@
-#include "chess.hpp"
+#include "chess_game.hpp"
+#include "chess_board.hpp"
+#include "chess_move.hpp"
+#include "chess_piece.hpp"
 
+#include <stack>
 #include <utility>
-// #include <vector>
+#include <vector>
 #include <iostream>
 
 // Implementation of ChessGame methods.
-namespace Chess {
-    ChessGame::ChessGame() {
-        while (!_chess_board_stack.empty()) {
-            _chess_board_stack.pop();
-        }
-        _chess_board_stack.push(ChessBoard());
-    }
 
-    std::vector<ChessMove> ChessGame::compute_legal_moves() {
-        return _chess_board_stack.top().compute_legal_moves();
-    }
-
-    std::vector<ChessMove> ChessGame::compute_legal_moves(int x, int y) {
-        return _chess_board_stack.top().compute_legal_moves(x, y);
-    }
-
-    void ChessGame::make_move(ChessMove chess_move) {
-        _chess_board_stack.push(ChessBoard(_chess_board_stack.top(), chess_move));
-    }
-
-    void ChessGame::undo_move() {
+ChessGame::ChessGame() {
+    while (!_chess_board_stack.empty()) {
         _chess_board_stack.pop();
     }
-
-    void ChessGame::print_board() {
-        _chess_board_stack.top().print_board();
-    }
-
-    bool ChessGame::is_check() {
-        return _chess_board_stack.top().is_check();
-    }
-
-
-
+    _current_board = new ChessBoard();
+    _chess_board_stack.push(_current_board);
+    _result = GameResult::IN_PROGRESS;
 }
+
+ChessGame::ChessGame(ChessBoard *chess_board) {
+    while (!_chess_board_stack.empty()) {
+        _chess_board_stack.pop();
+    }
+    _current_board = chess_board;
+    _chess_board_stack.push(_current_board);
+    // TODO: Compute result.
+}
+
+void ChessGame::make_move(ChessMove *chess_move) {
+    _current_board = new ChessBoard(_current_board, chess_move);
+    _chess_board_stack.push(_current_board);
+    // TODO: Compute game result.
+}
+
+void ChessGame::undo_move() {
+    _chess_board_stack.pop();
+    _current_board = _chess_board_stack.top();
+    _result = GameResult::IN_PROGRESS;
+}
+
+void ChessGame::print_board() {
+    _current_board->print_board();
+}
+
+bool ChessGame::has_game_ended() {
+    return _result != GameResult::IN_PROGRESS;
+}
+
+GameResult ChessGame::result() {
+    return _result;
+}
+
+bool ChessGame::is_white_turn() {
+    return _current_board->is_white_turn();
+}
+
+long long ChessGame::get_board_hash() {
+    return _current_board->get_board_hash();
+}
+
+
--- chess_game/chess_game.hpp
+++ chess_game/chess_game.hpp
@@ -5,52 +5,44 @@
 #include <vector>
 #include <stack>
 
-class ChessBoard {
-    public:
-        ChessBoard();
-        ChessBoard(ChessBoard previous_board, ChessMove chess_move);
-        long long get_board_hash();
-        std::vector<ChessMove> compute_legal_moves();
-        std::vector<ChessMove> compute_legal_moves(int x, int y);
-        void print_board();
-        bool is_white_turn();
-        char piece_at(int x, int y);
-        bool is_occupied(int x, int y);
-        bool is_check();
-        bool is_checkmate();
-        bool is_stalemate();
+#include "chess_move.hpp"
+#include "chess_board.hpp"
+#include "chess_globals.hpp"
+#include "chess_piece.hpp"
 
-        
-    private:
-        bool _can_white_big_castle;
-        bool _can_white_small_castle;
-        bool _can_black_big_castle;
-        bool _can_black_small_castle;
-        char _chess_board[8][8];
-        bool _white_turn;
-        int _num_moves;
-        int _num_moves_since_capture;
-        long long _board_hash;
-        void _reset_board();
+enum GameResult {
+    WHITE_WIN,
+    BLACK_WIN,
+    DRAW,
+    IN_PROGRESS
 };
 
 class ChessGame {
     public:
         ChessGame();
+        ChessGame(ChessBoard *chess_board);
+        
         std::vector<ChessMove> compute_legal_moves();
         std::vector<ChessMove> compute_legal_moves(int x, int y);
-        void make_move(ChessMove chess_move);
+        
+        void make_move(ChessMove *chess_move);
         void undo_move();
+        
         void print_board();
-        bool has_game_ended();
+        
         bool is_check();
-        int result();
+
+        bool has_game_ended();
+        GameResult result();
+        
         bool is_white_turn();
+
         long long get_board_hash();
         
     private:
-        std::stack<ChessBoard> _chess_board_stack;
-        bool _white_turn;
+        std::stack<ChessBoard*> _chess_board_stack;
+        ChessBoard *_current_board;
+        GameResult _result;
 };
 
 #endif
